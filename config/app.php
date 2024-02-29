f5ec17ff26d22c188d58bf5a9b94e88168c9e452
aekkakti <music.tomsk1@gmail.com>
2024-02-29 13:10:08 +0700

    Работа с безопасностью проекта

--- config/app.php
+++ config/app.php
@@ -2,8 +2,10 @@
 return [
     'auth' => \Src\Auth\Auth::class,
     'identity'=>\Model\User::class,
-    'routeMiddleware' => [
-        'auth' => \Middlewares\AuthMiddleware::class,
+    'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
     ],
     'validators' => [
         'required' => \Validators\RequireValidator::class,
